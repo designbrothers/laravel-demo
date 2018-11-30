3d7b0e718d2d7cd419473860b2f247ad3e9f2ac9
neurogas <neurogas@gmail.com>
2018-11-30 13:34:45 +0100

    edit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravello
                 </div>
 
                 <div class="links">
