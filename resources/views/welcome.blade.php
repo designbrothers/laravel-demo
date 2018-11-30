57e82a746e08f11e6f6d9efd989ac282c31c7b56
designbrothers <riccardo.s@fxdstudio.it>
2018-11-30 15:10:06 +0100

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    LaravelDay 2018
                 </div>
 
                 <div class="links">
